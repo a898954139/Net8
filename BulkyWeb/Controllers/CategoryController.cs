317b2aadcdd07d373ef292b1dacafa8da1e831c1
AnthonyKao <a898954139@gmail.com>
2024-02-09 21:53:41 +0800

    [Category] Implement notification partial view and notifications of all features

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,3 +1,4 @@
+using System.Configuration;
 using System.Data.SqlClient;
 using System.Diagnostics.CodeAnalysis;
 using System.Text.RegularExpressions;
@@ -15,63 +16,94 @@ public class CategoryController : Controller
     private readonly ApplicationDbContext _db;
     private readonly IRepository _repository;
 
+    # region Constructer
+
     public CategoryController(ApplicationDbContext db, IRepository repository)
     {
         _db = db;
         _repository = repository;
     }
 
+    # endregion
+
     public IActionResult Index()
     {
         var category = _db.Categories.ToList();
         return View(category);
-    }    
+    }
+
     public IActionResult Create()
     {
         return View();
-    }  
+    }
+
     [HttpPost]
     public IActionResult Create(Category category)
     {
-        if (ModelState.IsValid)
+        if (!ModelState.IsValid)
         {
-            // EfCoreInsert(category);
-            _repository.DapperInsert(category);
-            return RedirectToAction("Index");
+            if (Regex.IsMatch(category.Name, @"(?i)fuck"))
+                ModelState.AddModelError("name", "Contains sensitive keywords");
+            return View();
         }
-        if (Regex.IsMatch(category.Name, @"(?i)fuck"))
-            ModelState.AddModelError("name", "Contains sensitive keywords");
-        return View();
+
+        TryInsertValue(category);
+        return RedirectToAction("Index");
     }
+
     public IActionResult Edit(int? id)
     {
         if (id is null or 0)
             return NotFound();
         return View(_repository.GetCategoryByIdDapper(id));
     }
+
     [HttpPost]
     public IActionResult Edit(Category category)
     {
-        if (!ModelState.IsValid) 
+        if (!ModelState.IsValid)
             return View();
-        _db.Update(category);
-        _db.SaveChanges();
+        TryUpdateCategory(category);
         return RedirectToAction("Index");
     }
+
     public IActionResult Delete(int? id)
     {
         if (id is null or 0)
             return NotFound();
         return View(_repository.GetCategoryByIdDapper(id));
-    }    
+    }
+
     [HttpPost, ActionName("Delete")]
     public IActionResult PostDelete(int? id)
     {
-        if (id is null or 0)
+        if (IsInvalidId(id))
             return NotFound();
-        // _repository.DeleteCategoryByIdDapper(id);
-        DeleteCategoryByIdEfCore(id);
+        TryDeleteCategory(id);
         return RedirectToAction("Index");
+    }
+
+
+    # region private methods
+
+    private void TryDeleteCategory(int? id)
+    {
+        try
+        {
+            // _repository.DeleteCategoryByIdDapper(id);
+            DeleteCategoryByIdEfCore(id);
+            TempData["success"] = "Category deleted successfully";
+        }
+        catch (Exception e)
+        {
+            TempData["error"] = e.Message;
+            throw;
+        }
+    }
+
+    private static bool IsInvalidId(int? id)
+    {
+        return id is null or 0;
     }
 
     private void DeleteCategoryByIdEfCore(int? id)
@@ -80,14 +112,47 @@ public class CategoryController : Controller
         _db.SaveChanges();
     }
 
+    private void TryInsertValue(Category category)
+    {
+        try
+        {
+            // EfCoreInsert(category);
+            _repository.DapperInsert(category);
+            TempData["success"] = "Category successfully created.";
+        }
+        catch (Exception e)
+        {
+            TempData["error"] = e.Message;
+            throw;
+        }
+    }
+
+    private void TryUpdateCategory(Category category)
+    {
+        try
+        {
+            _db.Update(category);
+            TempData["success"] = "Category updated successfully.";
+            _db.SaveChanges();
+        }
+        catch (Exception e)
+        {
+            TempData["error"] = e.Message;
+            throw;
+        }
+    }
+
     private void InsertCategoryEfCore(Category category)
     {
         _db.Add(category);
         _db.SaveChanges();
     }
+
     private Category GetCategoryByIdEfCore([DisallowNull] int? id)
     {
         return _db.Categories.FirstOrDefault(x => x.Id == id)
-            ?? throw new Exception();
+               ?? throw new Exception();
     }
+
+    # endregion
 }
