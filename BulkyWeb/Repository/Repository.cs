dae620caf7136ca328d25fa2618c22b4a81805d2
Anthony <anthonykao.rd.xy@gmail.com>
2024-11-08 16:47:43 +0800

    [Repos] Implement IRepos

--- BulkyWeb/Repository/Repository.cs
+++ BulkyWeb/Repository/Repository.cs
@@ -1,39 +1,42 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Linq.Expressions;
+using Bulky.DataAccess.Data;
 using Bulky.Models.Models;
 using BulkyWeb.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.Repository;
 
-public class Repository<T> : IRepository<T> where T : class
+public class Repository<T>(
+    ApplicationDbContext db,
+    DbSet<T> dbSet) : 
+    IRepository<T> where T : class
 {
     public IEnumerable<T> GetAll()
     {
-        throw new NotImplementedException();
+        IQueryable<T> query = dbSet;
+        return query.ToList();
     }
 
     public T Get(Expression<Func<T, bool>> filter)
     {
-        throw new NotImplementedException();
+        IQueryable<T> query = dbSet; // Start with the DbSet
+        query = query.Where(filter); // Apply the filter
+        return query.FirstOrDefault(); 
     }
 
     public void Add(T entity)
     {
-        throw new NotImplementedException();
+        dbSet.Add(entity);
     }
-
-    public void Update(T entity)
-    {
-        throw new NotImplementedException();
-    }
-
+    
     public void Delete(T entity)
     {
-        throw new NotImplementedException();
+        dbSet.Remove(entity);
     }
 
     public void DeleteRange(IEnumerable<T> entity)
     {
-        throw new NotImplementedException();
+        dbSet.RemoveRange(entity);
     }
 }
